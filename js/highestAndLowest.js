8ab877803bf5b22971e59d3393262ea9b6598591
Gennadiy <borchuga@gmail.com>
2021-04-13 22:05:58 +0300

    Highest and Lowest

---
+++ js/highestAndLowest.js
@@ -0,0 +1,6 @@
+// 'https://www.codewars.com/kata/554b4ac871d6813a03000035/train/javascript'
+function highAndLow(numbers) {
+  numbers = numbers.split(' ');
+  return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+}
+console.log(highAndLow('4 5 29 54 4 0 -214 542 -64 1 -3 6 -6'));
