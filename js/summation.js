b1daea588c22c875cf4341b3cb092daf49180e03
Gennadiy <borchuga@gmail.com>
2021-04-13 16:21:22 +0300

    minor code fix

--- js/summation.js
+++ js/summation.js
@@ -1,10 +1,8 @@
 // 'https://www.codewars.com/kata/55d24f55d7dd296eb9000030/train/javascript'
 var summation = function (num) {
-  let counter = 0;
   let sum = 0;
-  while (counter !== num) {
-    counter += 1;
-    sum += counter;
+  for (let i = 0; i <= num; i += 1) {
+    sum += i;
   }
   return sum;
 };
