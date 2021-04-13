3ece954f944ec085c396477222b1af0c33ff0fd8
Gennadiy <borchuga@gmail.com>
2021-04-13 16:51:26 +0300

    Generate range of integers

---
+++ js/generateRangeOfIintegers.js
@@ -0,0 +1,10 @@
+// 'https://www.codewars.com/kata/55eca815d0d20962e1000106/train/javascript';
+function generateRange(min, max, step) {
+  let result = [];
+  for (min; min <= max; min += step) {
+    result.push(min);
+  }
+  return result;
+}
+console.log(generateRange(2, 10, 2));
+// [2,4,6,8,10]
