463c9e8598e5fbebecf7829ef92bc3a3eecc4780
Gennadiy <borchuga@gmail.com>
2021-04-15 08:20:57 +0300

    First non-repeating character

---
+++ js/firstNonRepeatingCharacter.js
@@ -0,0 +1,20 @@
+// 'https://www.codewars.com/kata/52bc74d4ac05d0945d00054e/train/javascript';
+
+function firstNonRepeatingLetter(s) {
+  let firstUnique;
+  const a = s.split('');
+  a.forEach(letter => {
+    const z = a.filter(item => item.toLowerCase() === letter.toLowerCase());
+    if (z.length === 1 && !firstUnique) {
+      firstUnique = letter;
+      return;
+    }
+  });
+  if (!firstUnique) {
+    firstUnique = '';
+  }
+  return firstUnique;
+}
+console.log(firstNonRepeatingLetter('a'));
+console.log(firstNonRepeatingLetter('stress'));
+console.log(firstNonRepeatingLetter('moonMen'));
